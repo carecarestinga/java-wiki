6b04cd902500b0e3779f4edfc5ca1213f8b18460
Pablo Hoties <pablohoties@gmail.com>
2018-12-31 13:15:53 -0200

    Cria Exercicio 01 da lista variaveis-e-tipos-de-dados

---
+++ exercicios/java-se/primeiros-passos/variaveis-e-tipos-de-dados/pablo-hoties/exercicio01/src/exercicio01/Main.java
@@ -0,0 +1,9 @@
+package exercicio01;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá Mundo!");
+		System.out.println("Seja bem-vindo ao mundo Java!");
+	}
+}
